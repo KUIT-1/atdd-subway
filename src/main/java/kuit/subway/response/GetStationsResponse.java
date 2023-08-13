b0550ca12b7b1c9c090f6958b368c067d3ae2726
kwiyoon <jiyun0245@gmail.com>
2023-08-14 00:19:12 +0900

    add (DTO) : create DTO for get stations

---
+++ src/main/java/kuit/subway/response/GetStationsResponse.java
@@ -0,0 +1,15 @@
+package kuit.subway.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetStationsResponse {
+    Long id;
+    String name;
+}
