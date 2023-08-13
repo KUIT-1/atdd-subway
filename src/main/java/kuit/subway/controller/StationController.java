2132171fc76b8fb75b62b3da6a80daa1521426e5
leesangjin <tkdwls0628@gmail.com>
2023-08-13 20:38:41 +0900

    [Feat]: Station 삭제 API 구현

--- src/main/java/kuit/subway/controller/StationController.java
+++ src/main/java/kuit/subway/controller/StationController.java
@@ -7,7 +7,9 @@ import kuit.subway.service.StationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +33,11 @@ public class StationController {
     public ResponseEntity<List<StationResponseDto>> showStations() {
         List<StationResponseDto> responseDtos = stationService.showStations();
         return ResponseEntity.ok(responseDtos);
+    }
+
+    @DeleteMapping("/stations/{stationId}")
+    public ResponseEntity<Void> deleteStation(@PathVariable Long stationId) {
+        stationService.deleteStation(stationId);
+        return ResponseEntity.status(204).build();
     }
 }
