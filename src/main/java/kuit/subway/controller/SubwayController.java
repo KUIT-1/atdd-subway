c7419804c408379c769344c5aa5311ce4617ecf1
kwiyoon <jiyun0245@gmail.com>
2023-08-14 00:02:42 +0900

    add (controller) : create SubwayController

---
+++ src/main/java/kuit/subway/controller/SubwayController.java
@@ -0,0 +1,12 @@
+package kuit.subway.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class SubwayController {
+}
