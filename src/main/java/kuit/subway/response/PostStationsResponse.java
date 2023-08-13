23274d030693a2c5c34e3adf3caa7e810c72ce77
kwiyoon <jiyun0245@gmail.com>
2023-08-14 00:07:11 +0900

    add (DTO) : create DTOs for post station

---
+++ src/main/java/kuit/subway/response/PostStationsResponse.java
@@ -0,0 +1,14 @@
+package kuit.subway.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostStationsResponse {
+    Long id;
+}
