488a8d589058939dfd76210dad972dc3a643ba5e
kwiyoon <jiyun0245@gmail.com>
2023-08-14 00:08:23 +0900

    feat : implement to create station

--- src/main/java/kuit/subway/controller/SubwayController.java
+++ src/main/java/kuit/subway/controller/SubwayController.java
@@ -1,7 +1,11 @@
 package kuit.subway.controller;
 
+import kuit.subway.request.PostStationRequest;
+import kuit.subway.response.PostStationsResponse;
+import kuit.subway.service.SubwayService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -9,4 +13,11 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 public class SubwayController {
+    private final SubwayService subwayService;
+
+    @PostMapping("/stations")
+    public PostStationsResponse createStation(@RequestBody PostStationRequest postStationRequest){
+        Long id = subwayService.createStation(postStationRequest.getName()).getId();
+        return new PostStationsResponse(id);
+    }
 }
--- src/main/java/kuit/subway/service/SubwayService.java
+++ src/main/java/kuit/subway/service/SubwayService.java
@@ -1,11 +1,20 @@
 package kuit.subway.service;
 
+import kuit.subway.domain.Subway;
+import kuit.subway.repository.SubwayRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class SubwayService {
+    private final SubwayRepository subwayRepository;
+
+    @Transactional
+    public Subway createStation(String name) {
+        return subwayRepository.save(new Subway(name));
+    }
 }
