833a72b56608d7a567424a7d4624d35365c9a992
kwiyoon <jiyun0245@gmail.com>
2023-08-14 00:17:57 +0900

    test : add api test for get stations

--- src/test/java/kuit/subway/AcceptanceTest.java
+++ src/test/java/kuit/subway/AcceptanceTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
@@ -44,5 +45,41 @@ public class AcceptanceTest {
 
         extract.statusCode();
         Assertions.assertEquals(200, extract.statusCode());
+    }
+
+    @Test
+    void getStations() {
+
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("name", "강남역");
+
+        RestAssured.given().contentType("application/json")
+                .body(jsonObj.toString())
+                .post("/stations")
+                .then().extract();
+
+        jsonObj.put("name", "건대입구역");
+
+        RestAssured.given().contentType("application/json")
+                .body(jsonObj.toString())
+                .post("/stations")
+                .then().extract();
+
+        ExtractableResponse<Response> extract = RestAssured.given().log().all()
+                        .when().get("/stations")
+                        .then().log().all()
+                        .extract();
+
+        Assertions.assertEquals(200, extract.statusCode());
+
+        List<String> stationNames = extract.jsonPath().getList("name");
+        List<Integer> stationId = extract.jsonPath().getList("id");
+
+        Assertions.assertEquals(1, stationId.get(0));
+        Assertions.assertEquals("강남역", stationNames.get(0));
+
+        Assertions.assertEquals(2, stationId.get(1));
+        Assertions.assertEquals("건대입구역", stationNames.get(1));
+
     }
 }
