ab149ae55fe77b830ee3d333cc5b7e8c77e181a1
kwiyoon <jiyun0245@gmail.com>
2023-08-14 00:20:50 +0900

    test : add api test for delete station

--- src/test/java/kuit/subway/AcceptanceTest.java
+++ src/test/java/kuit/subway/AcceptanceTest.java
@@ -82,4 +82,27 @@ public class AcceptanceTest {
         Assertions.assertEquals("건대입구역", stationNames.get(1));
 
     }
+
+    @Test
+    void deleteSubway() {
+
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("name", "강남역");
+
+        RestAssured.given().log().all()
+                .contentType("application/json")
+                .body(jsonObj.toString())
+                .when().post("/stations")
+                .then().log().all()
+                .extract();
+
+
+        ExtractableResponse<Response> extract =  RestAssured.given().log().all()
+                .when().delete("/stations/1")
+                .then().log().all()
+                .extract();
+
+        extract.statusCode();
+        Assertions.assertEquals(204, extract.statusCode());
+    }
 }
