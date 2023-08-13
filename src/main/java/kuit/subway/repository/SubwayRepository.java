a6f2d964c66e8f34d2485e926e527b73418ab622
kwiyoon <jiyun0245@gmail.com>
2023-08-14 00:04:13 +0900

    add (repository) : create SubwayRepository

---
+++ src/main/java/kuit/subway/repository/SubwayRepository.java
@@ -0,0 +1,8 @@
+package kuit.subway.repository;
+
+import kuit.subway.domain.Subway;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubwayRepository extends JpaRepository<Subway, Long> {
+
+}
