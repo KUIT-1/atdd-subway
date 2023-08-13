60852bfe23d6e5ee9201e2a503db578932b57ea7
kwiyoon <jiyun0245@gmail.com>
2023-08-14 00:01:38 +0900

    test : add api test for post station

--- src/test/java/kuit/subway/AcceptanceTest.java
+++ src/test/java/kuit/subway/AcceptanceTest.java
@@ -1,8 +1,13 @@
 package kuit.subway;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import kuit.subway.utils.DatabaseCleanup;
+import net.minidev.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -19,7 +24,25 @@ public class AcceptanceTest {
 
     @BeforeEach
     public void setUp() {
-        RestAssured.port = 443;
+        // 개발 환경 포트와 인수테스트 환경 포트 분리
+        RestAssured.port = port;
         databaseCleanup.execute();
+    }
+
+    @Test
+    void createStation() {
+
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("name", "강남역");
+
+        ExtractableResponse<Response> extract = RestAssured.given().log().all()
+                .contentType("application/json")
+                .body(jsonObj.toString())
+                .when().post("/stations")
+                .then().log().all()
+                .extract();
+
+        extract.statusCode();
+        Assertions.assertEquals(200, extract.statusCode());
     }
 }
