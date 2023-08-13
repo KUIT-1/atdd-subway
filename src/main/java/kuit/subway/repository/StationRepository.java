e7425dcbcc0b3353e6817c02d8a74a6bc313f737
leesangjin <tkdwls0628@gmail.com>
2023-08-13 21:02:39 +0900

    [Refactor]: 사용하지 않는 메서드 제거

--- src/main/java/kuit/subway/repository/StationRepository.java
+++ src/main/java/kuit/subway/repository/StationRepository.java
@@ -3,11 +3,7 @@ package kuit.subway.repository;
 import kuit.subway.entity.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface StationRepository extends JpaRepository<Station, Long> {
 
     boolean existsByName(String name);
-
-    Optional<Station> findByName(String name);
 }
