1249f3dfac8a260a7b24507d5dea17a38598fe70
leesangjin <tkdwls0628@gmail.com>
2023-08-14 19:46:26 +0900

    [Feat]: InvalidArgument를 담당하는 ExceptionHandler 추가

--- src/main/java/kuit/subway/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/kuit/subway/exception/handler/GlobalExceptionHandler.java
@@ -3,8 +3,11 @@ package kuit.subway.exception.handler;
 import kuit.subway.exception.ExceptionStatus;
 import kuit.subway.exception.SubwayException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static kuit.subway.exception.CustomExceptionStatus.INVALID_PARAMETER;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,6 +15,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(SubwayException.class)
     public ResponseEntity<Object> handleSubwayException(SubwayException e) {
         ExceptionStatus status = e.getStatus();
+        return ResponseEntity.status(status.getHttpStatus())
+                .body(status.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleInvalidArgumentException() {
+        ExceptionStatus status = INVALID_PARAMETER;
         return ResponseEntity.status(status.getHttpStatus())
                 .body(status.getMessage());
     }
