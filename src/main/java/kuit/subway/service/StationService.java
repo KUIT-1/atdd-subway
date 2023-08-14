107f12d9622c7a0df8ed0441e6805d95cd560a8e
leesangjin <tkdwls0628@gmail.com>
2023-08-14 19:41:55 +0900

    [Refactor]: CustomException을 통한 예외처리로 변경

--- src/main/java/kuit/subway/service/StationService.java
+++ src/main/java/kuit/subway/service/StationService.java
@@ -1,9 +1,10 @@
 package kuit.subway.service;
 
-import kuit.subway.dto.station.request.StationCreateRequestDto;
-import kuit.subway.dto.station.response.StationCreateResponseDto;
-import kuit.subway.dto.station.response.StationResponseDto;
+import kuit.subway.dto.station.request.StationCreateRequest;
+import kuit.subway.dto.station.response.StationCreateResponse;
+import kuit.subway.dto.station.response.StationResponse;
 import kuit.subway.entity.Station;
+import kuit.subway.exception.SubwayException;
 import kuit.subway.repository.StationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +13,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static kuit.subway.exception.CustomExceptionStatus.DUPLICATED_STATION;
+import static kuit.subway.exception.CustomExceptionStatus.NOT_EXISTED_STATION;
 
 @Service
 @Transactional(readOnly = true)
@@ -22,18 +26,18 @@ public class StationService {
     private final StationRepository stationRepository;
 
     @Transactional
-    public StationCreateResponseDto createStation(StationCreateRequestDto requestDto) {
+    public StationCreateResponse createStation(StationCreateRequest requestDto) {
         validateDuplicatedName(requestDto);
 
         Station station = stationRepository.save(requestDto.toEntity());
 
-        return StationCreateResponseDto.of(station);
+        return StationCreateResponse.of(station);
     }
 
-    public List<StationResponseDto> showStations() {
+    public List<StationResponse> showStations() {
         List<Station> stations = stationRepository.findAll();
-        List<StationResponseDto> responseDtos = stations.stream()
-                .map(StationResponseDto::of)
+        List<StationResponse> responseDtos = stations.stream()
+                .map(StationResponse::of)
                 .collect(Collectors.toList());
 
         return responseDtos;
@@ -42,14 +46,14 @@ public class StationService {
     @Transactional
     public void deleteStation(Long stationId) {
         Station station = stationRepository.findById(stationId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 역입니다. id=" + stationId));
+                .orElseThrow(() -> new SubwayException(NOT_EXISTED_STATION));
 
         stationRepository.delete(station);
     }
 
-    private void validateDuplicatedName(StationCreateRequestDto requestDto) {
+    private void validateDuplicatedName(StationCreateRequest requestDto) {
         if(stationRepository.existsByName(requestDto.getName())){
-            throw new IllegalArgumentException("이미 존재하는 역입니다. station=" + requestDto.getName());
+            throw new SubwayException(DUPLICATED_STATION);
         }
     }
 }
