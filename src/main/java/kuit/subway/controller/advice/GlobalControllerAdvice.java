891c26c305f077874bbdc318595b7e8f0665c886
kwiyoon <jiyun0245@gmail.com>
2023-08-14 22:16:25 +0900

    feat : add exception handler

--- src/main/java/kuit/subway/controller/advice/GlobalControllerAdvice.java
+++ src/main/java/kuit/subway/controller/advice/GlobalControllerAdvice.java
@@ -14,4 +14,26 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalControllerAdvice {
+    @ExceptionHandler(EntityExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String handleEntityExistsException(EntityExistsException ex) {
+        String field = "'" + ex.getMessage() + "'";
+        return "CONFLICT : " + field + " is already exists";
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        Map<String, Object> body = new HashMap<>();
+
+        List<String> errors = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(e -> e.getDefaultMessage())
+                .toList();
+        body.put("messages", errors);
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(body);
+    }
+
 }
