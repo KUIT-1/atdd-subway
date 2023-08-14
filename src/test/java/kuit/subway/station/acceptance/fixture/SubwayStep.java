24879c640f9c56cd093793e5fd591f2dfb2f2b04
leesangjin <tkdwls0628@gmail.com>
2023-08-14 22:36:19 +0900

    [Refactor]: Station과 관련된 RestAssured 관련 픽스쳐 분리

--- src/test/java/kuit/subway/station/acceptance/fixture/SubwayStep.java
+++ src/test/java/kuit/subway/station/acceptance/fixture/SubwayStep.java
@@ -2,15 +2,25 @@ package kuit.subway.station.acceptance.fixture;
 
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import kuit.subway.utils.RestAssuredUtils;
 import kuit.subway.station.dto.request.StationCreateRequest;
 
 import static kuit.subway.station.acceptance.fixture.SubwayFixture.지하철역_생성_요청;
+import static kuit.subway.utils.RestAssuredUtils.*;
 
 public class SubwayStep {
 
+    private static final String BASE_PATH = "/stations";
+
     public static ExtractableResponse<Response> 지하철역_생성(String name) {
         StationCreateRequest requestDto = 지하철역_생성_요청(name);
-        return RestAssuredUtils.post("/stations", requestDto);
+        return post(BASE_PATH, requestDto);
+    }
+
+    public static ExtractableResponse<Response> 지하철역_조회() {
+        return get(BASE_PATH);
+    }
+
+    public static ExtractableResponse<Response> 지하철역_삭제() {
+        return delete(BASE_PATH + "/{stationId}", 1L);
     }
 }
