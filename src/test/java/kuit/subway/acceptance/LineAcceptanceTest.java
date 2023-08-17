dfda59c6f85371518c8045e9d035f8cddfe20212
leesangjin <tkdwls0628@gmail.com>
2023-08-17 20:09:59 +0900

    [Feat]: Line 삭제 인수 테스트 구현

--- src/test/java/kuit/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/kuit/subway/acceptance/LineAcceptanceTest.java
@@ -32,7 +32,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
-    @DisplayName("검증조건을 만족시키지 않는 노선 생성 요청의 경우, 예외를 발생한다.")
+    @DisplayName("검증조건을 만족시키지 않는 노선 생성 요청의 경우, 예외가 발생한다.")
     @Test
     void createLine_Throw_Exception_If_Invalid_Request(){
         //given
@@ -47,7 +47,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
-    @DisplayName("상행종점역과 하행종점역이 같은 노선 생성 요청의 경우, 예외를 발생한다.")
+    @DisplayName("상행종점역과 하행종점역이 같은 노선 생성 요청의 경우, 예외가 발생한다.")
     @Test
     void createLine_Throw_Exception_If_Duplicated_Stations_Request(){
         //given
@@ -81,7 +81,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         );
     }
 
-    @DisplayName("존재하지 않는 지하철 노선을 조회시, 예외를 발생한다.")
+    @DisplayName("존재하지 않는 지하철 노선을 조회시, 예외가 발생한다.")
     @Test
     void showLines_Throw_Exception_If_Not_Existed_Line(){
         //given
@@ -111,7 +111,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
-    @DisplayName("존재하지 않는 지하철 노선을 수정시, 예외를 발생한다.")
+    @DisplayName("존재하지 않는 지하철 노선을 수정시, 예외가 발생한다.")
     @Test
     void updateLine_Throw_Exception_If_Not_Existed_Line(){
         //given
@@ -121,6 +121,36 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
         //when
         ExtractableResponse<Response> response = 노선_변경(2L, 노선_요청("신분당선", "red", 10L, 2L, 1L));
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @DisplayName("지하철 노선을 삭제한다.")
+    @Test
+    void deleteLine(){
+        //given
+        지하철역_생성(지하철역_생성_요청("강남역"));
+        지하철역_생성(지하철역_생성_요청("성수역"));
+        노선_생성(노선_요청("경춘선", "green", 10L, 1L, 2L));
+
+        //when
+        ExtractableResponse<Response> response = 노선_삭제(1L);
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+
+    @DisplayName("존재하지 않는 지하철 노선을 삭제시, 예외가 발생한다.")
+    @Test
+    void deleteLine_Throw_Exception_If_Not_Existed_Line(){
+        //given
+        지하철역_생성(지하철역_생성_요청("강남역"));
+        지하철역_생성(지하철역_생성_요청("성수역"));
+        노선_생성(노선_요청("경춘선", "green", 10L, 1L, 2L));
+
+        //when
+        ExtractableResponse<Response> response = 노선_삭제(2L);
 
         //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
