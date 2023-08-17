3ffb58ed35a18e4981fec56ad5a9052b755cae08
leesangjin <tkdwls0628@gmail.com>
2023-08-17 18:24:50 +0900

    [Feat]: Line 생성 인수 테스트용 Fixture 구현

---
+++ src/test/java/kuit/subway/acceptance/fixtures/LineAcceptanceFixtures.java
@@ -0,0 +1,16 @@
+package kuit.subway.acceptance.fixtures;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import kuit.subway.line.dto.request.LineCreateRequest;
+
+import static kuit.subway.utils.RestAssuredUtils.post;
+
+public class LineAcceptanceFixtures {
+
+    private static final String BASE_PATH = "/lines";
+
+    public static ExtractableResponse<Response> 노선_생성(LineCreateRequest request) {
+        return post(BASE_PATH, request);
+    }
+}
