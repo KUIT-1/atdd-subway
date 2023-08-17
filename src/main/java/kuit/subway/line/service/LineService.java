b3f546b94b5e2ad4f32e71633d939c460e9ccca6
leesangjin <tkdwls0628@gmail.com>
2023-08-17 18:46:30 +0900

    [Feat]: Line 조회 기능 구현

--- src/main/java/kuit/subway/line/service/LineService.java
+++ src/main/java/kuit/subway/line/service/LineService.java
@@ -4,14 +4,19 @@ import kuit.subway.global.exception.SubwayException;
 import kuit.subway.line.domain.Line;
 import kuit.subway.line.dto.request.LineCreateRequest;
 import kuit.subway.line.dto.response.LineCreateResponse;
+import kuit.subway.line.dto.response.LineResponse;
 import kuit.subway.line.repository.LineRepository;
 import kuit.subway.station.domain.Station;
+import kuit.subway.station.dto.response.StationResponse;
 import kuit.subway.station.repository.StationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static kuit.subway.global.exception.CustomExceptionStatus.NOT_EXISTED_LINE;
 import static kuit.subway.global.exception.CustomExceptionStatus.NOT_EXISTED_STATION;
 
 @Service
@@ -36,5 +41,16 @@ public class LineService {
 
         lineRepository.save(line);
         return LineCreateResponse.of(line);
+    }
+
+    public LineResponse showLine(Long lineId) {
+        Line line = lineRepository.findById(lineId)
+                .orElseThrow(() -> new SubwayException(NOT_EXISTED_LINE));
+
+        List<StationResponse> stations = line.getStations().stream()
+                .map(StationResponse::of)
+                .toList();
+
+        return LineResponse.of(line, stations);
     }
 }
