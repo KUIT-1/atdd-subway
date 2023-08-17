0e417cf722276140005f4e1552384951577bdb17
leesangjin <tkdwls0628@gmail.com>
2023-08-17 18:51:17 +0900

    [Feat]: Line 조회 인수 테스트 구현

--- src/test/java/kuit/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/kuit/subway/acceptance/LineAcceptanceTest.java
@@ -6,11 +6,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
+import java.util.List;
+
 import static kuit.subway.acceptance.fixtures.LineAcceptanceFixtures.노선_생성;
+import static kuit.subway.acceptance.fixtures.LineAcceptanceFixtures.노선_조회;
 import static kuit.subway.acceptance.fixtures.StationAcceptanceFixtures.지하철역_생성;
 import static kuit.subway.utils.fixtures.LineFixtures.노선_생성_요청;
 import static kuit.subway.utils.fixtures.StationFixtures.지하철역_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class LineAcceptanceTest extends AcceptanceTest {
 
@@ -39,6 +43,40 @@ public class LineAcceptanceTest extends AcceptanceTest {
         //when
         ExtractableResponse<Response> response =
                 노선_생성(노선_생성_요청("A".repeat(11),"G".repeat(11) , -1L, 2L, 1L));
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @DisplayName("지하철 노선을 조회한다.")
+    @Test
+    void showLines(){
+        //given
+        지하철역_생성(지하철역_생성_요청("강남역"));
+        지하철역_생성(지하철역_생성_요청("성수역"));
+        노선_생성(노선_생성_요청("경춘선", "green", 10L, 1L, 2L));
+
+        //when
+        ExtractableResponse<Response> response = 노선_조회(1L);
+        List<Object> stations = response.jsonPath().getList("stations.");
+
+        //then
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                () -> assertThat(stations).hasSize(2)
+        );
+    }
+
+    @DisplayName("존재하지 않는 지하철 노선을 조회시, 예외를 발생한다.")
+    @Test
+    void showLines_Throw_Exception_If_Not_Existed_Line(){
+        //given
+        지하철역_생성(지하철역_생성_요청("강남역"));
+        지하철역_생성(지하철역_생성_요청("성수역"));
+        노선_생성(노선_생성_요청("경춘선", "green", 10L, 1L, 2L));
+
+        //when
+        ExtractableResponse<Response> response = 노선_조회(2L);
 
         //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
