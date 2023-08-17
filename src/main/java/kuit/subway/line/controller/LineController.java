38f5b4427bd1e9dd37db1abda453318a778c17a5
leesangjin <tkdwls0628@gmail.com>
2023-08-17 20:09:18 +0900

    [Feat]: Line 삭제 API 구현

--- src/main/java/kuit/subway/line/controller/LineController.java
+++ src/main/java/kuit/subway/line/controller/LineController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,5 +42,11 @@ public class LineController {
                                                    @Valid @RequestBody LineRequest request) {
         LineResponse response = lineService.updateLine(lineId, request);
         return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/lines/{lineId}")
+    public ResponseEntity<Void> deleteLine(@PathVariable Long lineId) {
+        lineService.deleteLine(lineId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
