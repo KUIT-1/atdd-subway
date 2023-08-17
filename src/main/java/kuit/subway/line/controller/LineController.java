1301b982ab13a1395854753de061810e6cb4d50e
leesangjin <tkdwls0628@gmail.com>
2023-08-17 18:46:38 +0900

    [Feat]: Line 조회 API 구현

--- src/main/java/kuit/subway/line/controller/LineController.java
+++ src/main/java/kuit/subway/line/controller/LineController.java
@@ -3,11 +3,14 @@ package kuit.subway.line.controller;
 import jakarta.validation.Valid;
 import kuit.subway.line.dto.request.LineCreateRequest;
 import kuit.subway.line.dto.response.LineCreateResponse;
+import kuit.subway.line.dto.response.LineResponse;
 import kuit.subway.line.service.LineService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +25,13 @@ public class LineController {
     @PostMapping("/lines")
     public ResponseEntity<LineCreateResponse> createLine(@Valid @RequestBody LineCreateRequest request) {
         LineCreateResponse response = lineService.createLine(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(response);
+    }
+
+    @GetMapping("/lines/{lineId}")
+    public ResponseEntity<LineResponse> showLine(@PathVariable Long lineId) {
+        LineResponse response = lineService.showLine(lineId);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(response);
     }
