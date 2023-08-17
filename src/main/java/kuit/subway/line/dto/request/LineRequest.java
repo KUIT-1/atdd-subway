638404cce67ada7bc480b6f0571e451d78c09de5
leesangjin <tkdwls0628@gmail.com>
2023-08-17 19:26:36 +0900

    [Refactor]: Request 이름 변경 및 생성메서드 파라미터에 List<Station> 타입 추가

--- src/main/java/kuit/subway/line/dto/request/LineRequest.java
+++ src/main/java/kuit/subway/line/dto/request/LineRequest.java
@@ -4,17 +4,20 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import kuit.subway.line.domain.Line;
+import kuit.subway.station.domain.Station;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class LineCreateRequest {
+public class LineRequest {
 
     @Size(min = 2, max = 10)
     private String name;
@@ -27,11 +30,12 @@ public class LineCreateRequest {
     private Long downStationId;
     private Long upStationId;
 
-    public Line toEntity() {
+    public Line toEntity(List<Station> stations) {
         return Line.builder()
                 .name(name)
                 .color(color)
                 .distance(distance)
+                .stations(stations)
                 .build();
     }
 }
