aea64c9accfba4fb18afb18705b65bc6cb094b99
leesangjin <tkdwls0628@gmail.com>
2023-08-17 18:26:35 +0900

    [Refactor]: 파라미터 순서 변경

--- src/test/java/kuit/subway/utils/fixtures/LineFixtures.java
+++ src/test/java/kuit/subway/utils/fixtures/LineFixtures.java
@@ -4,11 +4,11 @@ import kuit.subway.line.dto.request.LineCreateRequest;
 
 public class LineFixtures {
 
-    public static LineCreateRequest 노선_생성_요청(String color, Long distance, String name, Long downStationId, Long upStationId) {
+    public static LineCreateRequest 노선_생성_요청(String name, String color, Long distance, Long downStationId, Long upStationId) {
         return LineCreateRequest.builder()
+                .name(name)
                 .color(color)
                 .distance(distance)
-                .name(name)
                 .downStationId(downStationId)
                 .upStationId(upStationId)
                 .build();
