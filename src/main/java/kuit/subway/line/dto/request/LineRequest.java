0ee575836900453c780b30a449c36776d0a8b92e
leesangjin <tkdwls0628@gmail.com>
2023-08-20 02:14:29 +0900

    [Refactor]: Dto -> Line 메서드 변경

--- src/main/java/kuit/subway/line/dto/request/LineRequest.java
+++ src/main/java/kuit/subway/line/dto/request/LineRequest.java
@@ -4,14 +4,11 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import kuit.subway.line.domain.Line;
-import kuit.subway.station.domain.Station;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Builder
 @Getter
@@ -26,16 +23,13 @@ public class LineRequest {
     @NotNull
     @Min(1)
     private Long distance;
-
     private Long downStationId;
     private Long upStationId;
 
-    public Line toEntity(List<Station> stations) {
+    public Line toEntity() {
         return Line.builder()
                 .name(name)
                 .color(color)
-                .distance(distance)
-                .stations(stations)
                 .build();
     }
 }
