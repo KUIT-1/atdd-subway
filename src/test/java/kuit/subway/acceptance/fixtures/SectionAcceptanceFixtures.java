46fa2432acf1efde3394be098a5001210a1dd3c1
leesangjin <tkdwls0628@gmail.com>
2023-08-20 02:43:19 +0900

    [Feat]: Section 관련 Fixtures 구현

---
+++ src/test/java/kuit/subway/acceptance/fixtures/SectionAcceptanceFixtures.java
@@ -0,0 +1,16 @@
+package kuit.subway.acceptance.fixtures;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import kuit.subway.line.dto.request.SectionRequest;
+
+import static kuit.subway.utils.RestAssuredUtils.post;
+
+public class SectionAcceptanceFixtures {
+
+    private static final String BASE_URL = "/lines/{lineId}/sections";
+
+    public static ExtractableResponse<Response> 구간_생성(Long lineId, SectionRequest request) {
+        return post(request, BASE_URL, lineId);
+    }
+}
