af7dcbb25abdda9e79c61f162d5f282fa12a6dc8
leesangjin <tkdwls0628@gmail.com>
2023-08-20 02:17:02 +0900

    [Refactor]: 연관관계 변경 및 불필요한 메서드 제거

--- src/main/java/kuit/subway/station/domain/Station.java
+++ src/main/java/kuit/subway/station/domain/Station.java
@@ -2,13 +2,9 @@ package kuit.subway.station.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import kuit.subway.line.domain.Line;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,12 +25,4 @@ public class Station {
 
     @Column(length = 20, nullable = false)
     private String name;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "line_id")
-    private Line line;
-
-    public void addLine(Line line) {
-        this.line = line;
-    }
 }
