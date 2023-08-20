ad09d6d6e06c97b5914aa502ab989be0e2ae6c62
kwiyoon <jiyun0245@gmail.com>
2023-08-21 04:28:37 +0900

    feat : create LineRequest DTO with validation

---
+++ src/main/java/kuit/subway/request/line/LineRequest.java
@@ -0,0 +1,33 @@
+package kuit.subway.request.line;
+
+
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LineRequest {
+    @Pattern(regexp = "[a-zA-Z]+", message = "영문")
+    String color;
+
+    @Positive
+    Long distance;
+
+    @Pattern(regexp = "[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣1-9]+", message = "영문, 한글, 숫자만 가능")
+    @Length(min = 1, max = 20, message = "1 ~ 20자리 이내")
+    String name;
+
+    @Positive
+    Long downStationId;
+
+    @Positive
+    Long upStationId;
+
+}
