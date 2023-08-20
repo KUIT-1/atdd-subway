359f7045b48dcea3f9d1ebe869727d3c0e8819ea
kwiyoon <jiyun0245@gmail.com>
2023-08-21 04:24:02 +0900

    create : add LineRepository

--- src/main/java/kuit/subway/service/LineService.java
+++ src/main/java/kuit/subway/service/LineService.java
@@ -1,7 +1,9 @@
 package kuit.subway.service;
+import kuit.subway.repository.LineRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class LineService {
+    private final LineRepository lineRepository;
 }
