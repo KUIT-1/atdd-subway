09a24e86d74785bf2a3a1f33e5a4bc6193b3f6f9
leesangjin <tkdwls0628@gmail.com>
2023-08-21 16:48:53 +0900

    [Refactor]: 기존 메서드 일급 컬렉션으로 이동 및 일급 컬렉션의 메서드를 호출하도록 변경

--- src/main/java/kuit/subway/line/domain/Line.java
+++ src/main/java/kuit/subway/line/domain/Line.java
@@ -1,14 +1,12 @@
 package kuit.subway.line.domain;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import kuit.subway.BaseTimeEntity;
-import kuit.subway.global.exception.SubwayException;
 import kuit.subway.station.domain.Station;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -17,10 +15,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import static kuit.subway.global.exception.CustomExceptionStatus.*;
 
 @Entity
 @Getter
@@ -40,8 +35,8 @@ public class Line extends BaseTimeEntity {
     @Column(length = 10, nullable = false)
     private String color;
 
-    @OneToMany(mappedBy = "line", cascade = CascadeType.ALL, orphanRemoval = true)
-    public List<Section> sections = new ArrayList<>();
+    @Embedded
+    public Sections sections = new Sections();
 
     @Builder
     public Line(String name, String color) {
@@ -55,57 +50,14 @@ public class Line extends BaseTimeEntity {
     }
 
     public void addSection(Section section) {
-        validateAvailableSection(section);
-        this.sections.add(section);
-    }
-
-    // TODO 리팩토링 필수
-    public List<Station> getStations() {
-        List<Station> stations = new ArrayList<>();
-        boolean isFirst = true;
-        for (Section section : sections) {
-            if (isFirst) {
-                stations.add(section.getUpStation());
-                stations.add(section.getDownStation());
-            } else {
-                stations.add(section.getDownStation());
-            }
-            isFirst = false;
-        }
-        return stations;
+        sections.addSection(section);
     }
 
     public void removeSection() {
-        if (sections.size() == 1) {
-            throw new SubwayException(CANNOT_REMOVE_SECTION);
-        }
-        sections.remove(sections.size() - 1);
+        sections.removeSection();
     }
 
-    private void validateAvailableSection(Section section) {
-        // 처음 역을 만드는 경우
-        if (sections.size() == 0) {
-            return;
-        }
-        validateExistedDownStation(section);
-        validateCycleInDownStation(section);
-    }
-
-    // 새로운 구간의 상행역은 등록되어있는 하행 종점역이어야 한다.
-    private void validateExistedDownStation(Section section) {
-        Section lastSection = sections.get(sections.size() - 1);
-        if (!lastSection.getDownStation().getId().equals(section.getUpStation().getId())) {
-            throw new SubwayException(INVALID_SECTION_NOT_EXISTED_DOWN_STATION);
-        }
-    }
-
-    // 새로운 구간의 하행역은 해당 노선에 등록되어있는 역일 수 없다.
-    private void validateCycleInDownStation(Section section) {
-        boolean hasCycle = sections.stream().anyMatch(existedSection ->
-                existedSection.getUpStation() == section.getDownStation() || existedSection.getDownStation() == section.getDownStation());
-
-        if (hasCycle) {
-            throw new SubwayException(INVALID_SECTION_CANNOT_CYCLE);
-        }
+    public List<Station> getStations() {
+        return sections.getStations();
     }
 }
