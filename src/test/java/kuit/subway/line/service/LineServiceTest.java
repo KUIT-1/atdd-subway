5c4bc676e6bc870b6a816832817379b31c818f09
leesangjin <tkdwls0628@gmail.com>
2023-08-23 18:13:53 +0900

    [Feat]: Line 조회 테스트코드 구현

--- src/test/java/kuit/subway/line/service/LineServiceTest.java
+++ src/test/java/kuit/subway/line/service/LineServiceTest.java
@@ -2,6 +2,7 @@ package kuit.subway.line.service;
 
 import kuit.subway.line.domain.Line;
 import kuit.subway.line.dto.response.LineCreateResponse;
+import kuit.subway.line.dto.response.LineResponse;
 import kuit.subway.line.repository.LineRepository;
 import kuit.subway.station.domain.Station;
 import kuit.subway.station.repository.StationRepository;
@@ -16,6 +17,7 @@ import java.util.Optional;
 
 import static kuit.subway.utils.fixtures.LineFixtures.노선_요청;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
@@ -59,4 +61,24 @@ class LineServiceTest {
         assertThat(result.getId()).isEqualTo(경춘선.getId());
     }
 
+    @DisplayName("라인을 조회한다.")
+    @Test
+    void showLine(){
+        //given
+        Line line = Line.builder().id(1L).name("경춘선").color("green").build();
+
+        given(lineRepository.findById(1L))
+                .willReturn(Optional.ofNullable(line));
+
+        //when
+        LineResponse response = lineService.showLine(1L);
+
+        //then
+        verify(lineRepository, times(1)).findById(anyLong());
+        assertAll(
+                () -> assertThat(response.getId()).isEqualTo(1L),
+                () -> assertThat(response.getName()).isEqualTo(line.getName()),
+                () -> assertThat(response.getColor()).isEqualTo(line.getColor())
+        );
+    }
 }
