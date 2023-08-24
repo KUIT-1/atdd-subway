5e3bbe508426cea3d0153464fcd4e73b716cb860
kwiyoon <jiyun0245@gmail.com>
2023-08-25 05:00:56 +0900

    feat : implement section registration validation

--- src/main/java/kuit/subway/domain/Line.java
+++ src/main/java/kuit/subway/domain/Line.java
@@ -42,4 +42,28 @@ public class Line {
         this.downStation = downStation;
     }
 
+    private final static String DOWN = "down";
+    private final static String UP = "up";
+
+    public void isSectionRegistrable(Long upStationId, Long downStationId) {
+        checkStationIsLastDownStation(upStationId, UP);
+        checkDownStationIsNonexistent(downStationId);
+    }
+    private void checkStationIsLastDownStation(Long stationId, String direction) {
+        Station lastDownStation = getLastDownStation();
+
+        if(Objects.equals(direction, UP)
+                & !Objects.equals(lastDownStation.getId(), stationId)){
+            throw new LineException(ONLY_LAST_DOWNSTATION_REGISTER_ALLOWED);
+        }
+        if(Objects.equals(direction, DOWN)
+                & !Objects.equals(lastDownStation.getId(), stationId)){
+            throw new LineException(ONLY_LAST_SECTION_DELETION_ALLOWED);
+        }
+    }
+
+    private void checkDownStationIsNonexistent(Long downStationId) {
+        boolean isExist = sections.hasStation(downStationId);
+        if(isExist) throw new LineException(ALREADY_REGISTERED_STATION);
+    }
 }
