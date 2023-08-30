968d08a79d836cdd260e22000e849af35d85b5fe
leesangjin <tkdwls0628@gmail.com>
2023-08-30 22:58:36 +0900

    [Refactor]: PathVariable 대신 QueryString으로 변경

--- src/main/java/kuit/subway/line/controller/LineController.java
+++ src/main/java/kuit/subway/line/controller/LineController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -62,9 +63,9 @@ public class LineController {
                 .body(response);
     }
 
-    @DeleteMapping("/{lineId}/sections/{stationId}")
+    @DeleteMapping("/{lineId}/sections")
     public ResponseEntity<LineResponse> deleteSection(@PathVariable Long lineId,
-                                                      @PathVariable Long stationId) {
+                                                      @RequestParam Long stationId) {
         LineResponse response = lineService.deleteSection(lineId, stationId);
         return ResponseEntity.ok(response);
     }
