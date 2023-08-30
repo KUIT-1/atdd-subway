ae9f88266f6e481b928e29e4590bba5dac0de0e7
leesangjin <tkdwls0628@gmail.com>
2023-08-31 01:51:30 +0900

    [Feat]: 지하철 노선 중, 원하는 구간을 반환하는 기능 구현

--- src/main/java/kuit/subway/path/domain/SubwayMap.java
+++ src/main/java/kuit/subway/path/domain/SubwayMap.java
@@ -1,6 +1,9 @@
 package kuit.subway.path.domain;
 
+import kuit.subway.global.exception.SubwayException;
 import kuit.subway.line.domain.Line;
+import kuit.subway.line.domain.Section;
+import kuit.subway.line.domain.Sections;
 import kuit.subway.station.domain.Station;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -8,9 +11,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.WeightedMultigraph;
 
 import java.util.List;
+import java.util.Optional;
+
+import static kuit.subway.global.exception.CustomExceptionStatus.NOT_EXISTED_PATH;
 
 @Builder
 @Getter
@@ -20,6 +28,23 @@ import java.util.List;
 public class SubwayMap {
 
     private List<Line> lines;
+
+    public Sections findPath(Station sourceStation, Station targetStation) {
+        WeightedMultigraph<Station, SectionEdge> graph = generateSubwayMap();
+        DijkstraShortestPath<Station, SectionEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
+        Optional<GraphPath<Station, SectionEdge>> pathOptional = Optional.ofNullable(dijkstraShortestPath.getPath(sourceStation, targetStation));
+
+        if (pathOptional.isEmpty()) {
+            throw new SubwayException(NOT_EXISTED_PATH);
+        }
+
+        GraphPath<Station, SectionEdge> path = pathOptional.get();
+        List<Section> sections = path.getEdgeList().stream()
+                .map(SectionEdge::getSection)
+                .toList();
+
+        return new Sections(sections);
+    }
 
     private WeightedMultigraph<Station, SectionEdge> generateSubwayMap() {
         WeightedMultigraph<Station, SectionEdge> graph = new WeightedMultigraph<>(SectionEdge.class);
