4172309bcc017b84c37c2dcf37c0ef8143b9ec12
kwiyoon <jiyun0245@gmail.com>
2023-09-03 17:52:27 +0900

    refactor : 검증 추가 - notnull

--- src/test/java/kuit/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/kuit/subway/acceptance/SectionAcceptanceTest.java
@@ -4,8 +4,11 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import kuit.subway.request.section.DeleteSectionRequest;
 import kuit.subway.request.section.SectionRequest;
+import kuit.subway.utils.exception.LineException;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import static kuit.subway.acceptance.fixtures.LineStep.지하철_2호선_생성_요청;
 import static kuit.subway.acceptance.fixtures.SectionStep.지하철_구간_등록_요청;
@@ -14,6 +17,7 @@ import static kuit.subway.acceptance.fixtures.StationFixture.*;
 import static kuit.subway.acceptance.fixtures.StationStep.지하철_역_생성_요청;
 import static kuit.subway.utils.BaseResponseStatus.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SectionAcceptanceTest extends AcceptanceTest {
@@ -89,6 +93,21 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         assertThat(response.jsonPath().getList("result.stations"))
                 .extracting("name")
                 .containsExactly("성수역", "건대역", "강남역");
+    }
+
+    @DisplayName("구간 추가 - request 값들이 모두 null인 경우")
+    @Test
+    void 모든_요청값_null() {
+        // given
+        SectionRequest request = new SectionRequest(null, null,null);
+        // when
+        ExtractableResponse<Response> response = 지하철_구간_등록_요청("1", request);
+        // then
+        assertThat(response.jsonPath().getList("messages"))
+                .containsOnly("새로운 구간의 거리 필요",
+                        "새로운 구간의 하행역 id 필요",
+                        "새로운 구간의 상행역 id 필요");
+
     }
 
 
