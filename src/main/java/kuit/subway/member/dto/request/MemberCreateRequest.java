19043281ad8f6e5e6f2b4f89c9acb7d9d5771b77
leesangjin <tkdwls0628@gmail.com>
2023-09-06 01:35:09 +0900

    [Feat]: Member 회원가입 관련 request / response 추가

---
+++ src/main/java/kuit/subway/member/dto/request/MemberCreateRequest.java
@@ -0,0 +1,38 @@
+package kuit.subway.member.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import kuit.subway.member.domain.Member;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberCreateRequest {
+
+    @NotBlank(message = "아이디를 입력해주세요.")
+    @Pattern(regexp = "^[a-z0-9]{4,16}", message = "이름은 4-16자의 영어, 또는 숫자로 입력해주세요.")
+    private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Pattern(regexp = "^(?=.*[~!@#$%^&*()_+])[A-Za-z0-9~!@#$%^&*()_+]{8,}", message = "특수문자 포함 8자 이상 입력해야 합니다.")
+    private String password;
+
+    @NotBlank(message = "나이를 입력해주세요.")
+    @Positive
+    private Integer age;
+
+    public Member toEntity() {
+        return Member.builder()
+                .email(email)
+                .password(password)
+                .age(age)
+                .build();
+    }
+}
