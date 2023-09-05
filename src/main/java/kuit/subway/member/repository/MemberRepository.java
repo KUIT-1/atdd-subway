699ff4e7aeda4db0eeff9a780a9e53693b86c628
leesangjin <tkdwls0628@gmail.com>
2023-09-06 01:35:33 +0900

    [Feat]: MemberRepository 초기 구현 및 이메일 존재 여부 쿼리 메서드 추가

---
+++ src/main/java/kuit/subway/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package kuit.subway.member.repository;
+
+import kuit.subway.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsByEmail(String email);
+}
+
