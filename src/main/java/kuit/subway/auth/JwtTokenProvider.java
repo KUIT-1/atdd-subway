0dccd05ffaf94a7467058a51479fd45eeb933bde
leesangjin <tkdwls0628@gmail.com>
2023-09-06 22:29:53 +0900

    [Feat]: 예외처리 추가

--- src/main/java/kuit/subway/auth/JwtTokenProvider.java
+++ src/main/java/kuit/subway/auth/JwtTokenProvider.java
@@ -1,22 +1,27 @@
 package kuit.subway.auth;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import kuit.subway.global.exception.SubwayException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
+import static kuit.subway.global.exception.CustomExceptionStatus.EXPIRED_ACCESS_TOKEN;
+import static kuit.subway.global.exception.CustomExceptionStatus.INVALID_ACCESS_TOKEN;
+
 @Component
 public class JwtTokenProvider {
     private final String secretKey;
     private final long validityInMilliseconds;
-    private final JwtParser jwtParser;
 
     public JwtTokenProvider(@Value("${security.jwt.token.secret-key}") String secretKey,
                             @Value("${security.jwt.token.expire-length}") long validityInMilliseconds) {
         this.secretKey = secretKey;
         this.validityInMilliseconds = validityInMilliseconds;
-        this.jwtParser = Jwts.parser().setSigningKey(secretKey);
     }
 
     public String createToken(Long memberId) {
@@ -31,26 +36,29 @@ public class JwtTokenProvider {
                 .compact();
     }
 
-    // TODO : 예외 처리
     public void validateToken(String token) {
         try {
-            jwtParser.parseClaimsJws(token);
+            Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(token);
         } catch (ExpiredJwtException e) {
-//            throw new TokenExpiredException();
+            throw new SubwayException(EXPIRED_ACCESS_TOKEN);
         } catch (JwtException e) {
-//            throw new InvalidTokenException();
+            throw new SubwayException(INVALID_ACCESS_TOKEN);
         }
     }
 
     public String getPayload(String token) {
         try {
-            return jwtParser.parseClaimsJws(token).getBody().getSubject();
+            return Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject();
         } catch (ExpiredJwtException e) {
-//            throw new TokenExpiredException();
+            throw new SubwayException(EXPIRED_ACCESS_TOKEN);
         } catch (JwtException e) {
-//            throw new InvalidTokenException();
+            throw new SubwayException(INVALID_ACCESS_TOKEN);
         }
-        // 임시
-        return "";
     }
 }
