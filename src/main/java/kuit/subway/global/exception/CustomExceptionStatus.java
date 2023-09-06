7034db9cd6343e36d311c8f408133f58eae3722c
leesangjin <tkdwls0628@gmail.com>
2023-09-06 23:02:42 +0900

    [Feat]: Member 관련 CustomException 추가

--- src/main/java/kuit/subway/global/exception/CustomExceptionStatus.java
+++ src/main/java/kuit/subway/global/exception/CustomExceptionStatus.java
@@ -35,7 +35,8 @@ public enum CustomExceptionStatus implements ExceptionStatus {
 
     // member exception
     NOT_EXISTED_MEMBER(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
-    DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다."),
+    DUPLICATED_EMAIL(HttpStatus.NOT_FOUND, "이미 존재하는 이메일입니다."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "잘못된 비밀번호 입니다."),
 
     // jwt exception
     EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 AccessToken 입니다."),
