e362bdff66202076fecf5a40d623a9e91e212f87
leesangjin <tkdwls0628@gmail.com>
2023-09-07 20:28:07 +0900

    [Feat]: Github userInfo 관련 Client 구현

---
+++ src/main/java/kuit/subway/auth/service/github/GithubUserInfoClient.java
@@ -0,0 +1,12 @@
+package kuit.subway.auth.service.github;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "github-userinfo-client", url = "https://api.github.com/user")
+public interface GithubUserInfoClient {
+
+    @GetMapping
+    GithubUserInfo getUserInfo(@RequestHeader(name = "Authorization") String authorization);
+}
