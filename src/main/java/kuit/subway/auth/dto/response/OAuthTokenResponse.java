97ff1bbd98e2c95a35eb9a299c41c5bd604426ae
leesangjin <tkdwls0628@gmail.com>
2023-09-07 20:26:01 +0900

    [Feat]: AccessToken 발급을 위한 request/resopnse 구현

---
+++ src/main/java/kuit/subway/auth/dto/response/OAuthTokenResponse.java
@@ -0,0 +1,18 @@
+package kuit.subway.auth.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OAuthTokenResponse {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+}
