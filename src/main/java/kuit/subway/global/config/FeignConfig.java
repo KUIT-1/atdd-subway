cc99420ac9c6448683facad8ac05aa290b98be74
leesangjin <tkdwls0628@gmail.com>
2023-09-07 20:30:02 +0900

    [Refactor]: FeignConfig 삭제 및 SubwayApplication에 어노테이션 추가

--- src/main/java/kuit/subway/global/config/FeignConfig.java
+++
@@ -1,8 +0,0 @@
-package kuit.subway.global.config;
-
-import kuit.subway.SubwayApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-
-@EnableFeignClients(basePackageClasses = SubwayApplication.class)
-public class FeignConfig {
-}
