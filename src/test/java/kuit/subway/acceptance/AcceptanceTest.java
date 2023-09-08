f4bb9e5b444db6caaf7179847529192988741d13
leesangjin <tkdwls0628@gmail.com>
2023-09-08 21:41:07 +0900

    [Feat]: ExternalConfig 설정 추가

--- src/test/java/kuit/subway/acceptance/AcceptanceTest.java
+++ src/test/java/kuit/subway/acceptance/AcceptanceTest.java
@@ -2,14 +2,17 @@ package kuit.subway.acceptance;
 
 import io.restassured.RestAssured;
 import kuit.subway.utils.DatabaseCleanup;
+import kuit.subway.utils.ExternalConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(ExternalConfig.class)
 public abstract class AcceptanceTest {
 
     @Autowired
